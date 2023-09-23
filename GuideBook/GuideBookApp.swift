72c657b95a6ae4827cad8a7c53266073f2b7dbb8
Houman <veryhouman@gmail.com>
2023-09-23 09:55:00 -0400

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Houman Azari on 2023-09-23.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
